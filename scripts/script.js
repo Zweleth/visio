c2be0d37b6f442346ef9d68f96c137acd449bf24
Zweleth <zwebeko@gmail.com>
2023-10-11 14:32:08 +0200

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+$(".slider > div:gt(0)").hide();
+
+setInterval(function () {
+  $(".slider > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo(".slider");
+}, 7000);
+
+
+
+
+$(".dot1").click(function () {
+  $(".slider > div:first").hide();
+  $(".slider > .obj1").fadeIn(1000);
+  alert('hello')
+
+});
+$(".dot2").click(function () {
+  $(".slider > div:first").hide();
+  $(".slider > .obj2").fadeIn(1000);
+  alert('hello')
+
+});
+$(".dot3").click(function () {
+  $(".slider > div:first").hide();
+  $(".slider > .obj3").fadeIn(1000);
+
+
+});
+$(".dot4").click(function () {
+  $(".slider > div:first").hide();
+  $(".slider > .obj4").fadeIn(1000);
+  alert('hello')
+
+});
+
+
+$(".next").click(function () {
+  // action goes here!!
+
+  $(".slider > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo(".slider");
+});
